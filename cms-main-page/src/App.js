58a1e4a2ea0e2d3b753e6bdc8b0e0b52787a1cf3
= <=>
2024-05-26 10:12:39 +0800

    initial commit

---
+++ cms-main-page/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import cmsMainPage from './Components/cmsMainPageJS/cmsMainPage.jsx';
+
+function App() {
+  return (
+    <div>
+      <cmsMainPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ cms-main-page/src/Components/cmsMainPageJS/cmsMainPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const cmsMainPage = () => {
+  return (
+    <div>cmsMainPage</div>
+  )
+}
+
+export default cmsMainPage
